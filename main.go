c6d21c075faa2de08a6e49882f9133ae1f52586f
Keya Sukthankar <keya.sukthankar@gmail.com>
2020-03-19 12:44:52 +0530

    Complete Part1

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 func main() {
-	http.ListenAndServe(":9090")
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Hello World")
+		d, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(rw, "Oops", http.StatusBadRequest)
+			return
+		}
+		fmt.Fprintf(rw, "Hello %s", d)
+	})
+	http.ListenAndServe(":9090", nil)
 }
