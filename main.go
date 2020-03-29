4134dcac601a7d18b77365c9e4dbff01bcb70748
Keya Sukthankar <keya.sukthankar@gmail.com>
2020-03-29 19:27:30 +0530

    interface

--- main.go
+++ main.go
@@ -1,25 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 )
 
 func main() {
-	/* http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		log.Println("Hello World")
 		d, err := ioutil.ReadAll(r.Body)
 		if err != nil {
 			http.Error(rw, "Oops", http.StatusBadRequest)
 			return
 		}
-		fmt.Fprintf(rw, "Hello %s" , d)
-	}) */
-	l := log.New(os.Stdout, "product-api", log.LstdFlags)
-	hh := handlers.NewHello()
-
-	http.HandleFunc()
+		fmt.Fprintf(rw, "Hello %s", d)
+	})
 
 	http.ListenAndServe(":9090", nil)
 }
