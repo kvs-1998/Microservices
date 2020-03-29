4bee64fc678ac6930def5c95436e0baa626efff2
Keya Sukthankar <keya.sukthankar@gmail.com>
2020-03-30 00:14:21 +0530

    changes

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"./handlers"
 )
@@ -11,11 +14,39 @@ import (
 func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
 	//servemux is an handler
 	//which handler registered against it get called
 	//and then just call servehttp func of the handler based on
 	// the request
 	sm := http.NewServeMux() //serve mux created
 	sm.Handle("/", hh)       //register path to hh-hello handler
-	http.ListenAndServe(":9090", nil)
+	sm.Handle("/Goodbye", gh)
+
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	} //Manually creating server
+
+	//s.ListenAndServe() //sm also implements the handler interface
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <- sigChan
+	l.Println("Received terminate, graceful shutdown", sig)
+
+	//To Forcefully close after 30 seconds
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc) //Server wont accept requests
 }
